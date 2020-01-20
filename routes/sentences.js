7e1415d2e1c5f888ac1685c00ae636db52d25d90
mateusz <mp371366@github.com>
2020-01-20 18:13:29 +0100

    Add sentence REST api.

--- routes/sentences.js
+++ routes/sentences.js
@@ -1,25 +1,70 @@
 import express from 'express';
+import Sentence from '../models/sentence.js';
+
 const router = express.Router();
 
-// Get all sentences
-router.get('/', (req, res) => {
-  res.send('Hello World')
-})
+async function getSentence(req, res, next) {
+  try {
+    const sentence = await Sentence.findById(req.params.id);
 
-// Get one sentence
-router.get('/:id', (req, res) => {
-})
+    if (sentence == null) {
+      return res.status(404).json({ message: 'Cant find sentence' });
+    }
 
-// Create one sentence
-router.post('/', (req, res) => {
-})
+    res.sentence = sentence;
+  } catch ({ message }) {
+    return res.status(500).json({ message });
+  }
+  next();
+}
 
-// Update one sentence
-router.patch('/:id', (req, res) => {
-})
+router.get('/', async (req, res) => {
+  try {
+    const sentences = await Sentence.find();
 
-// Delete one sentence
-router.delete('/:id', (req, res) => {
-})
+    res.json(sentences);
+  } catch ({ message }) {
+    res.status(500).json({ message });
+  }
+});
+
+router.get('/:id', getSentence, (req, res) => {
+  res.json(res.sentence);
+});
+
+router.post('/', async (req, res) => {
+  const sentence = new Sentence({
+    text: req.body.text
+  });
+
+  try {
+    const newSsentence = await sentence.save();
+    res.status(201).json(newSsentence);
+  } catch ({ message }) {
+    res.status(400).json({ message });
+  }
+});
+
+router.patch('/:id', getSentence, async (req, res) => {
+  if (req.body.text != null) {
+    res.sentence.text = req.body.text;
+  }
+
+  try {
+    const updatedSentence = await res.sentence.save();
+    res.json(updatedSentence);
+  } catch ({ message }) {
+    res.status(400).json({ message });
+  }
+});
+
+router.delete('/:id', getSentence, async (req, res) => {
+  try {
+    await res.sentence.remove();
+    res.json({ message: 'Deleted this sentence.' });
+  } catch ({ message }) {
+    res.status(500).json({ message });
+  }
+});
 
 export default router;
