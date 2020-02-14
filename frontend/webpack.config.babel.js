627254e4819890dab3ed260765749ee4de7da949
mp371366 <mp371366@github.com>
2020-02-14 18:53:36 +0100

    Reorder react server webpack configuration.

--- frontend/webpack.config.babel.js
+++ frontend/webpack.config.babel.js
@@ -1,4 +1,4 @@
 const clientConfig = require('./webpack.client.config.babel');
 const serverConfig = require('./webpack.server.config.babel');
 
-module.exports = [clientConfig, serverConfig];
+module.exports = [serverConfig, clientConfig];
