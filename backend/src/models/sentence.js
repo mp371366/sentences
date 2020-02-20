c16ac8cd1fac3f7944c17f81ae13ab9c700bb7d1
mp371366 <mp371366@github.com>
2020-02-20 16:29:17 +0100

    Update model.

--- backend/src/models/sentence.js
+++ backend/src/models/sentence.js
@@ -5,29 +5,6 @@ const sentenceSchema = new mongoose.Schema({
     type: String,
     required: true,
   },
-  tags: {
-    type: Array
-  },
-  likes: {
-    type: Number,
-    required: true,
-    default: 0
-  },
-  dislakes: {
-    type: Number,
-    required: true,
-    default: 0
-  },
-  viewed: {
-    type: Number,
-    required: true,
-    default: 0
-  },
-  added: {
-    type: Date,
-    required: true,
-    default: Date.now
-  }
 });
 
 export default mongoose.model('Sentence', sentenceSchema);
